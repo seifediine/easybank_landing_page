99dffd02b1bd28dee4fd27b9c1682cbe3cedff20
Himouzzi <seifeddine.elkorichi@hotmil.com>
2020-05-27 00:19:46 +0100

    added animated hamburger menu & overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,17 @@
-console.log("Hello Pinazza");
+const hamburgerBtn = document.querySelector("#hamburger-btn");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+
+hamburgerBtn.addEventListener("click", function () {
+  console.log("Hello Himou !!");
+
+  if (header.classList.contains("open")) {
+    header.classList.remove("open"); // Open Hamburger Menu
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
+  } else {
+    header.classList.add("open"); // Close Hamburger Menu
+    overlay.classList.add("fade-in");
+    overlay.classList.remove("fade-out");
+  }
+});
